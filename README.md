83697e82b47d63d2f6d081c054175c12679437a1
Vikash Kumar <devmrvicky.com>
2026-01-28 08:02:07 +0530

    configure node server and impliment rate limit

---
+++ README.md
@@ -0,0 +1,27 @@
+## AI Voice Detection API
+
+### Problem
+
+Detect whether a voice sample is AI-generated or human.
+
+### Approach
+
+- Audio-based acoustic feature extraction
+- Language-agnostic analysis
+- Binary ML classifier
+
+### Supported Languages
+
+- Tamil
+- English
+- Hindi
+- Malayalam
+- Telugu
+
+### Input
+
+Base64-encoded MP3 audio
+
+### Output
+
+Classification + confidence score
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { rateLimit } from "express-rate-limit";
+
+dotenv.config({});
+
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
+
+app.use(limiter);
+
+app.get("/test", (req, res) => {
+  res.send({ server_testing: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`🚀server running on port : ${port}`);
+});
